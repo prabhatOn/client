56efc468efb3227034d7a477b1ac11ce0b828075
prabhatOn <prabhat@ssipmt.com>
2025-10-15 12:52:25 +0530

    google tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,15 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-D9ZPCKZ1SH"></script>
+        <script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-D9ZPCKZ1SH');
+        ` }} />
+        
         {/* Favicon */}
         <link rel="icon" href="/dp-logo.jpg" />
         <link rel="shortcut icon" href="/dp-logo.jpg" />
