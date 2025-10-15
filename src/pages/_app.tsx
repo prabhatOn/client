f2ce0c886206e9cdf2aa30f9e1bfc089ed092c09
prabhatOn <prabhat@ssipmt.com>
2025-10-15 13:03:31 +0530

    analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@
 import { AppProps } from 'next/app';
 import '@/styles/globals.css';
 import MainLayout from '@/components/layout/MainLayout';
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +11,8 @@ function MyApp({ Component, pageProps }: AppProps) {
       <div className='bg-neutral-50 min-h-screen'>
         <Component {...pageProps} />
       </div>
+      <Analytics />
+      <SpeedInsights />
     </MainLayout>
   );
 }
